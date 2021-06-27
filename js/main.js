a063729efa62bdd44c71613fe4f87f35801ee13c
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2021-06-27 22:57:18 +0800

    Фикс input range отступов в input + фикс моб меню

--- js/main.js
+++ js/main.js
@@ -53,6 +53,10 @@ $(document).ready(function() {
         var top = $(id).offset().top;
 
         $('body, html').animate({scrollTop: top}, 1500);
+
+        $('.burger-btn').removeClass('burger-btn--activ');
+        $('.burger-btn__item').removeClass('burger-btn__item--activ');
+        $('.mobile').removeClass('mobile--activ');
     });
 
     $('#mob-experience').on("click", function(event) {
@@ -62,6 +66,10 @@ $(document).ready(function() {
         var top = $(id).offset().top;
 
         $('body, html').animate({scrollTop: top}, 2000);
+
+        $('.burger-btn').removeClass('burger-btn--activ');
+        $('.burger-btn__item').removeClass('burger-btn__item--activ');
+        $('.mobile').removeClass('mobile--activ');
     });
 
     $('#mob-js').on("click", function(event) {
@@ -71,6 +79,10 @@ $(document).ready(function() {
         var top = $(id).offset().top;
 
         $('body, html').animate({scrollTop: top}, 2500);
+
+        $('.burger-btn').removeClass('burger-btn--activ');
+        $('.burger-btn__item').removeClass('burger-btn__item--activ');
+        $('.mobile').removeClass('mobile--activ');
     });
 
     $('#mob-about').on("click", function(event) {
@@ -80,13 +92,56 @@ $(document).ready(function() {
         var top = $(id).offset().top;
 
         $('body, html').animate({scrollTop: top}, 3000);
+
+        $('.burger-btn').removeClass('burger-btn--activ');
+        $('.burger-btn__item').removeClass('burger-btn__item--activ');
+        $('.mobile').removeClass('mobile--activ');
+    });
+
+
+    // Ползунок
+    var item1 = 0;
+    var item2 = 20;
+    var item3 = 50;
+    var item4 = 100;
+
+    $("#polzunok").slider({
+        animate: "slow",
+        range: "min",    
+        value: 60,
+        slide : function(event, ui) {            
+            $("#send-result-polzunok").val(ui.value);    
+        }
+    });
+    $("#send-result-polzunok").val($("#polzunok").slider("value")); 
+
+
+    // Переход по нажатию на указатель
+    $('#item-1').on("click", function(event) {
+        event.preventDefault();
+
+        $('#polzunok').slider("value", item1);       
+        $("#send-result-polzunok").val(item1);
+    });
+
+    $('#item-2').on("click", function(event) {
+        event.preventDefault();
+
+        $('#polzunok').slider("value", item2);      
+        $("#send-result-polzunok").val(item2);
+    });
+
+    $('#item-3').on("click", function(event) {
+        event.preventDefault();
+
+        $('#polzunok').slider("value", item3);     
+        $("#send-result-polzunok").val(item3);
+    });
+
+    $('#item-4').on("click", function(event) {
+        event.preventDefault();
+
+        $('#polzunok').slider("value", item4);       
+        $("#send-result-polzunok").val(item4);
     });
 });
-
-// input range для хром, яндекс, опера
-function rangeInput() {
-    var value = $('.range').val();
-    console.log(value);  
-
-    $('.range').css({'background': '-webkit-linear-gradient(left, #435993 0%, #435993 '+value+'%, #e9e9e9 '+value+'%, #e9e9e9 100%)'});
-}
